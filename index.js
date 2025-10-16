c8f9ba24d2475870b5f771414b4c117719bba6d4
Denning Kahky <denning.dk@gmail.com>
2025-10-16 14:31:35 +0100

    increase up size2

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors({
 
 // Body parsing middleware
 app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 // Routes
 app.use('/api/auth', require('./src/routes/auth'));
